499ad060cb421c7d524940210f2d9b6068d7e3ac
SAMUEL MOTA <samuelhpmota@gmail.com>
2025-12-28 18:47:43 -0300

    Middleware de linguagem

---
+++ src/cashflow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace cashflow.Api.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+        //recebo o idioma encaminhado na requisição
+        var cultureQuery = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+        //define o idioma defualt
+        var cultureInfo = new CultureInfo("en-US");
+        //verifico se foi encaminhado algum idioma na requisição, se sim, altero o idioma default para o encaminhado
+        if (!string.IsNullOrWhiteSpace(cultureQuery))
+        {
+            cultureInfo = new CultureInfo(cultureQuery);
+        }
+        //defino o idioma da aplicação
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+        //continua o processamento da requisição
+        await _next(context);
+    }
+
+}
--- src/cashflow.Api/Program.cs
+++ src/cashflow.Api/Program.cs
@@ -1,4 +1,5 @@
 using cashflow.Api.Filters;
+using cashflow.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
