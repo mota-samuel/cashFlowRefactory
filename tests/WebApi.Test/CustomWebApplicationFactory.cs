10c0f2c07e64be469833a5fe39733d272d4a491a
SAMUEL MOTA <samuelhpmota@gmail.com>
2026-08-04 05:16:51 -0300

    creating DB in memory

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApi.Test;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
     override protected void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.UseEnvironment("Test");
+        builder.UseEnvironment("Test").
+            ConfigureServices(services =>
+            {
+                services.AddDbContext<CashFlowDbContext>
+            });
     }
 
 
